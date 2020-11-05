5dcc4b6330ca76c39427e95f0112c52aa80df109
manitejav <teja3536mani@gmail.com>
2020-11-05 16:56:02 +0530

    markdown basic syntax tranformer plus sphinx node api

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -7,20 +7,35 @@ from lark import Lark
 example_md = """
 # Heading1
 That is paragraph text.
-# Heading2
+## Heading2
 This is a paragraph2.
-"""
+*Italic*
+**Bold**
 
+[Link](http://a.com)
+![Image](http://url/a.png)
+> Blockquote
+* List
+* List
+* List
+1. One
+2. Two
+3. Three
+
+`Inline code` with backticks
+```
+# code block
+print '3 backticks or'
+```
+"""
 
 parser = Parser()
 example_md_ast = parser.parse(example_md)
-# print(md_parser.parse(example_md).pretty())
 
 def test_md_ast_get_children():
     children = _md_ast_children(example_md_ast)
-    assert [x.t for x in children] == [
-        'heading', 'paragraph', 'heading', 'paragraph'
-    ]
+    print([x.t for x in children])
+    assert [x.t for x in children] == ['heading', 'paragraph', 'heading', 'paragraph', 'paragraph', 'block_quote', 'list', 'list', 'paragraph', 'code_block']
 
 
 def test_markdown_to_tree():
@@ -35,6 +50,6 @@ def test_tree_to_sphinx_node():
 	print(node)	
 
 if __name__ == "__main__":
-    # test_md_ast_get_children()
-    # test_markdown_to_tree()
+    test_md_ast_get_children()
+    test_markdown_to_tree()
     test_tree_to_sphinx_node()
